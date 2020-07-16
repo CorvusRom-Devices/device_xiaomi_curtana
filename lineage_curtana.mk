477b9b231dfeb50ddc9b01d8ee2c6880af7332a9
Demon000 <demonsingur@gmail.com>
2020-07-16 18:18:47 +0300

    curtana: set model to global one

--- lineage_curtana.mk
+++ lineage_curtana.mk
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_curtana
 PRODUCT_DEVICE := curtana
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi Note 9 Pro
+PRODUCT_MODEL := Redmi Note 9S
 PRODUCT_MANUFACTURER := Redmi
 
 BUILD_FINGERPRINT := "Redmi/curtana/curtana:10/QKQ1.191215.002/V11.0.4.0.QJWINXM:user/release-keys"
